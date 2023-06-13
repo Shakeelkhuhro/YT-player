faf5093f5d2caaaa4aad47833c0516760355fa3d
Shakeel Khuhro <shakeelahmedkhuhro5454@gmail.com>
2023-06-13 22:56:00 +0500

    path updated to YT-player

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
     <div className="App">
       <Router>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/yt-player" element={<Home />} />
           <Route path="/courses/:coursename" element={<CourseStructure />} />
         </Routes>
       </Router>
